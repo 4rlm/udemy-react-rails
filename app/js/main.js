931f34367769c3e021692fd4dfc2fe93f8faf42a
Adam Booth <4rlm@protonmail.ch>
2017-11-08 18:52:38 -0600

    section 15, lecture 119 at 2:25 min.

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
-const app = document.getElementById('app');
+// const app = document.getElementById('app');
+// app.innerHTML = "Hello ES6!"
 
-app.innerHTML = "Hello ES6!"
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+  <p>Hello React.</p>,
+  <p>I'm another p!</p>,
+
+  document.getElementById('app')
+);
