2cda0cb1be62e56608f93339509b24df476c9517
Adam Booth <4rlm@protonmail.ch>
2017-11-21 14:44:06 -0600

    created a timer component being re-used in three places.

--- app/js/components/Layout.js
+++ app/js/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Button from './Button';
+import Timer from './Timer';
 
 export default class Layout extends React.Component {
   constructor() {
@@ -24,7 +25,10 @@ export default class Layout extends React.Component {
         <h3>Happy to have you here.</h3>
         <h1>{this.state.firstName}</h1>
         <Button firstName={this.state.firstName} changeName={this.changeName.bind(this)}/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur temporibus distinctio deleniti officia labore ut quod eum rem aperiam sequi, possimus fuga tempore, unde recusandae fugit. Voluptatibus illum ad aperiam!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur temporibus distinctio deleniti.</p>
+        <Timer />
+        <Timer />
+        <Timer />
       </div>
     )
   }
