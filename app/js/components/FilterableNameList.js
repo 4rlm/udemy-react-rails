59565f2f84be3908c5dbe6c689039ed2181b1e18
Adam Booth <4rlm@protonmail.ch>
2017-11-22 12:21:08 -0600

    good save point.  renamed NameList to FilterableNameList.

--- app/js/components/FilterableNameList.js
+++ app/js/components/FilterableNameList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class NameList extends React.Component {
+export default class FilterableNameList extends React.Component {
   constructor() {
     super();
     this.state = {
--- app/js/components/Layout.js
+++ app/js/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import NameList from './NameList';
+import FilterableNameList from './FilterableNameList';
 import Timer from './Timer';
 
 export default class Layout extends React.Component {
@@ -15,7 +15,8 @@ export default class Layout extends React.Component {
         <h3>Happy to have you here.</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur temporibus distinctio deleniti.</p>
         <Timer />
-        <NameList names={this.props.names} />
+        <FilterableNameList names={this.props.names} />
+
       </div>
     )
   }
