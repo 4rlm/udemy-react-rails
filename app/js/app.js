2ae8d05f40e201584322f2013d411fd2f820ede1
Adam Booth <4rlm@protonmail.ch>
2017-11-23 02:59:22 -0600

    Lesson 146: WildCard * for all non routes redirect to 404 page.

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,7 @@ import { Router, Route, browserHistory } from 'react-router';
 import Layout from './components/Layout';
 import Timer from './components/Timer';
 import FilterableNameList from './components/FilterableNameList';
+import PageNotFound from './components/PageNotFound';
 
 
 ReactDom.render(
@@ -12,6 +13,7 @@ ReactDom.render(
     <Route path="/" component={Layout} >
       <Route path="timer" component={Timer} />
       <Route path="nameList" component={FilterableNameList} />
+      <Route path="*" component={PageNotFound} />
     </Route>
   </Router>,
   document.getElementById('app')
